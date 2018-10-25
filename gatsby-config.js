5f343af263f215886659eed9d9d8e9c2d5e743e6
Ashwin <aswinjeganath@gmail.com>
2018-10-25 15:43:17 +0530

    Set up classes collection

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,40 @@
 module.exports = {
-	siteMetadata: {
-		title: 'Eastside Swim School',
-	},
-	plugins: [
-		'gatsby-plugin-react-helmet',
-		`gatsby-plugin-styled-components`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: `${__dirname}/src/images`,
-			},
-		},
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-sharp',
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: 'gatsby-starter-default',
-				short_name: 'starter',
-				start_url: '/',
-				background_color: '#663399',
-				theme_color: '#663399',
-				display: 'minimal-ui',
-				icon: 'src/images/evelationlogo.png', // This path is relative to the root of the site.
-			},
-		},
-		'gatsby-plugin-offline',
-		'gatsby-plugin-netlify-cms',
-	],
-};
+  siteMetadata: {
+    title: 'Eastside Swim School',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/evelationlogo.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify-cms',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `testimonials`,
+        path: `${__dirname}/testimonial/`,
+      },
+    },
+    'gatsby-transformer-remark',
+  ],
+}
