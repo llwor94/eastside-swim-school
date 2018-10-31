b78fa43f995d88843e77ddce52bdf5092140ae7e
Lauren <llwor94@gmail.com>
2018-10-30 18:48:15 -0700

    ditched uncessary imports

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -2,12 +2,6 @@ import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
 import GridList from '@material-ui/core/GridList';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
