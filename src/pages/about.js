46bd65c947fbb755fd4d059203c9b9170d187f59
Lauren <llwor94@gmail.com>
2018-11-04 15:14:34 -0800

    styled about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,60 +1,85 @@
 import React from 'react';
-
+import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
-
+import Paper from '@material-ui/core/Paper';
 import Layout from '../components/Layout';
 import Lisa from '../images/lisa.png';
 
 const Wrapper = styled.div`
 	margin: 0 30px;
-
 	padding-top: 100px;
-
 	max-width: 960px;
 	margin: 0 auto;
+
+	img {
+		float: right;
+		width: 50%;
+		border-radius: 0 3px 0 0;
+		padding-left: 10px;
+	}
 `;
+
+const TextWrapper = styled.div`padding: 10px;`;
 
 const About = () => (
 	<Layout>
 		<Wrapper>
-			<img src={Lisa} alt="" />
-			<h1>Lisa Worthington</h1>
-			<p>
-				Lisa is the business owner, swim instructor and triathlon coach. She has been teaching swim lessons for
-				over 30 years and competing in triathlons of all distances for 25 years . Lisa learned to swim at a
-				young age, competed thru the collegiate level and has continued swimming competitively thru her adult
-				life.
-			</p>
-			<p>
-				Lisa has grown up with a passion for the water and teaching others how to swim. She love to teach the
-				new beginner and watch them advance. She has grown Eastside Swim school and hired additional
-				professional instructors to teach under the “eastside style”.
-			</p>
-			<p>
-				Lisa has raised two children, volunteered in classrooms and taught children swimming, and loves kids!
-				She is comfortable with babies, and kids of all ages and abilities.
-			</p>
-			<p>
-				Lisa has tremendous experience with the advanced level swimmer. She has experience with age group
-				coaching, Masters swimming as well as triathlon specific swimming. Lisa offers a wide array of services
-				for the advanced swimmer, she utilizes an underwater camera to analyze and correct a swimmers stroke and
-				technique.
-			</p>
-			<p>
-				Lisa is currently the head triathlon coach for the University of Washington Husky Tri Team. She also is
-				the head coach for the Masters swim program at the Gold Creek Club. Lisa is the co-founder/owner of
-				Finish Strong Events (a local events company) as well as the founder and Manager of Elevation
-				Multisport, a local triathlon club.
-			</p>
-			<p>
-				Besides swimming, Lisa is also an avid cyclist and currently road races with a local bike team. Current
-				certifications include:USA Triathlon Coach Level 2, USA Cycling Coach Level 3, Adult Learn to Swim,
-				Heart Zones Cycling and Personal Training, Certified Lifeguard, First Aid, and CPR.
-			</p>
-			<p>Lisa teaches both at her private residence and Gold Creek Club in Woodinville.</p>
-			{/* <img src={instructors} alt="" />
+			<Paper>
+				<img src={Lisa} alt='' />
+				<TextWrapper>
+					<Typography gutterBottom variant='h2' component='h2'>
+						Lisa Worthington
+					</Typography>
+					<Typography gutterBottom variant='body1' component='div'>
+						<p>
+							Lisa is the business owner, swim instructor and triathlon coach. She has
+							been teaching swim lessons for over 30 years and competing in triathlons
+							of all distances for 25 years . Lisa learned to swim at a young age,
+							competed thru the collegiate level and has continued swimming
+							competitively thru her adult life.
+						</p>
+						<p>
+							Lisa has grown up with a passion for the water and teaching others how
+							to swim. She love to teach the new beginner and watch them advance. She
+							has grown Eastside Swim school and hired additional professional
+							instructors to teach under the “eastside style”.
+						</p>
+						<p>
+							Lisa has raised two children, volunteered in classrooms and taught
+							children swimming, and loves kids! She is comfortable with babies, and
+							kids of all ages and abilities.
+						</p>
+						<p>
+							Lisa has tremendous experience with the advanced level swimmer. She has
+							experience with age group coaching, Masters swimming as well as
+							triathlon specific swimming. Lisa offers a wide array of services for
+							the advanced swimmer, she utilizes an underwater camera to analyze and
+							correct a swimmers stroke and technique.
+						</p>
+						<p>
+							Lisa is currently the head triathlon coach for the University of
+							Washington Husky Tri Team. She also is the head coach for the Masters
+							swim program at the Gold Creek Club. Lisa is the co-founder/owner of
+							Finish Strong Events (a local events company) as well as the founder and
+							Manager of Elevation Multisport, a local triathlon club.
+						</p>
+						<p>
+							Besides swimming, Lisa is also an avid cyclist and currently road races
+							with a local bike team. Current certifications include:USA Triathlon
+							Coach Level 2, USA Cycling Coach Level 3, Adult Learn to Swim, Heart
+							Zones Cycling and Personal Training, Certified Lifeguard, First Aid, and
+							CPR.
+						</p>
+						<p>
+							Lisa teaches both at her private residence and Gold Creek Club in
+							Woodinville.
+						</p>
+						{/* <img src={instructors} alt="" />
 			<p>Deanna, Lisa, Tiffani and Lori. All professional adults with decades of experience.</p>
 			<Link to="/">Go back to the homepage</Link> */}
+					</Typography>
+				</TextWrapper>
+			</Paper>
 		</Wrapper>
 	</Layout>
 );
