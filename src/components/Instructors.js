ab25c3410dd66d8a5546cf737898a386a816b466
Ashwin <aswinjeganath@gmail.com>
2018-11-07 14:42:03 +0530

    Refactor instructors and testimonials section

--- src/components/Instructors.js
+++ src/components/Instructors.js
@@ -9,108 +9,162 @@ import Typography from '@material-ui/core/Typography';
 import styles from './Instructors.module.styl';
 
 const BottomBar = ({ styles, link }) => (
-	<div>
-		<Typography variant="h6" component="h4" className={styles.mr} gutterBottom>
-			Schedule Private Lessons
-		</Typography>
-		<div className={styles.bottomBar}>
-			<Button
-				variant="contained"
-				color="primary"
-				size="small"
-				href={link}
-				component="a"
-				className={styles.mr}
-				target="_blank"
-			>
-				1-on-1
-			</Button>
-			<Button variant="contained" color="primary" size="small" href={link} component="a" target="_blank">
-				1-on-2
-			</Button>
-		</div>
-	</div>
+  <div>
+    <Typography variant="h6" component="h4" className={styles.mr} gutterBottom>
+      Schedule Private Lessons
+    </Typography>
+    <div className={styles.bottomBar}>
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        href={link}
+        component="a"
+        className={styles.mr}
+        target="_blank"
+      >
+        1-on-1
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        href={link}
+        component="a"
+        target="_blank"
+      >
+        1-on-2
+      </Button>
+    </div>
+  </div>
 );
 
 const Instructors = () => (
-	<StaticQuery
-		query={graphql`
-			query {
-				allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/coaches/" } }) {
-					edges {
-						node {
-							html
-							frontmatter {
-								title
-								oneStudent
-								twoStudents
-								thumbnail
-								link
-							}
-						}
-					}
-				}
-			}
-		`}
-		render={({ allMarkdownRemark }) => {
-			// console.log(allMarkdownRemark);
-			// const { classes } = props
-			return (
-				<section className={styles.instructors} name="coaches">
-					<div className={styles.container}>
-						<Typography gutterBottom variant="h2" component="h2">
-							Private Lessons
-						</Typography>
-						<Paper className={styles.instructor}>
-							<Grid container spacing={40}>
-								{allMarkdownRemark.edges.map(
-									(
-										{
-											node: {
-												frontmatter: { thumbnail, title, oneStudent, twoStudents, link },
-												html,
-											},
-										},
-										idx,
-									) => (
-										<Grid className={styles.bottomLineWrapper} item xs={12}>
-											<Grid container spacing={40} className={styles.bottomLine}>
-												<Grid item className={styles.grow} xs={12} sm={7} md={8}>
-													<div className={styles.wrapper}>
-														<Typography gutterBottom variant="h5" component="h3">
-															{title}
-														</Typography>
-														<Typography
-															gutterBottom
-															variant="body1"
-															component="div"
-															dangerouslySetInnerHTML={{ __html: html }}
-														/>
-														<BottomBar styles={styles} link={link} />
-													</div>
-												</Grid>
-												<Grid
-													item
-													className={`${idx % 2 === 0 && styles.first} ${styles.pic}`}
-													xs={12}
-													sm={5}
-													md={4}
-												>
-													<div className={styles.imgWrapper}>
-														<img src={thumbnail} alt={title} className={styles.img} />
-													</div>
-												</Grid>
-											</Grid>
-										</Grid>
-									),
-								)}
-							</Grid>
-						</Paper>
-					</div>
-				</section>
-			);
-		}}
-	/>
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex: "/coaches/" } }
+        ) {
+          edges {
+            node {
+              html
+              frontmatter {
+                title
+                oneStudent
+                twoStudents
+                thumbnail
+                link
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={({ allMarkdownRemark }) => {
+      // console.log(allMarkdownRemark);
+      // const { classes } = props
+      allMarkdownRemark.edges.sort(
+        (
+          {
+            node: {
+              frontmatter: { title: title1 },
+            },
+          },
+          {
+            node: {
+              frontmatter: { title: title2 },
+            },
+          },
+        ) => {
+          if (title1 === 'Lisa') return -1;
+          if (title2 === 'Lisa') return 1;
+
+          return title1.localeCompare(title2);
+        },
+      );
+      return (
+        <section className={styles.instructors} name="coaches">
+          <div className={styles.container}>
+            <Typography gutterBottom variant="h2" component="h2">
+              Private Lessons
+            </Typography>
+            <Paper className={styles.instructor}>
+              <Grid container spacing={40}>
+                {allMarkdownRemark.edges.map(
+                  (
+                    {
+                      node: {
+                        frontmatter: {
+                          thumbnail,
+                          title,
+                          oneStudent,
+                          twoStudents,
+                          link,
+                        },
+                        html,
+                      },
+                    },
+                    idx,
+                  ) => (
+                    <Grid className={styles.bottomLineWrapper} item xs={12}>
+                      <Grid
+                        container
+                        spacing={40}
+                        className={styles.bottomLine}
+                      >
+                        <Grid
+                          item
+                          className={styles.grow}
+                          xs={12}
+                          sm={7}
+                          md={8}
+                        >
+                          <div className={styles.wrapper}>
+                            <Typography
+                              gutterBottom
+                              variant="h5"
+                              component="h3"
+                            >
+                              {title}
+                            </Typography>
+                            <Typography
+                              gutterBottom
+                              variant="body1"
+                              component="div"
+                              dangerouslySetInnerHTML={{ __html: html }}
+                            />
+                            <BottomBar styles={styles} link={link} />
+                          </div>
+                        </Grid>
+                        <Grid
+                          item
+                          className={`${idx % 2 === 0 && styles.first} ${
+                            styles.pic
+                          }`}
+                          xs={12}
+                          sm={5}
+                          md={4}
+                        >
+                          <div className={styles.imgWrapper}>
+                            <img
+                              src={thumbnail}
+                              alt={title}
+                              className={styles.img}
+                            />
+                          </div>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  ),
+                )}
+              </Grid>
+            </Paper>
+          </div>
+        </section>
+      );
+    }}
+  />
 );
 
 export default Instructors;
