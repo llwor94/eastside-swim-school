f70a2555847b8efdd4efd9fdb03ab9a5e6c4cb18
Lauren <llwor94@gmail.com>
2018-11-07 19:09:11 -0800

    added email button to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Typography from '@material-ui/core/Typography';
 
 import './footer.styl';
 
 const Footer = () => (
-	<footer className="footer">
-		<Typography variant="subtitle1" className="author">
+	<footer className='footer'>
+		<Typography variant='subtitle1' className='author'>
 			Designed by{' '}
-			<a className="link" href="https://github.com/cooltable" target="_blank">
+			<a className='link' href='https://github.com/cooltable' target='_blank'>
 				Cool Table
 			</a>
 		</Typography>
-		<div className="icon">
-			<a href="https://www.facebook.com/eastsideswimschool/" target="_blank">
-				<FontAwesomeIcon icon={faFacebook} size="3x" />
-			</a>
+		<div className='icons'>
+			<div className='icon'>
+				<a href='mailto:eastsideswimschool@comcast.net'>
+					<FontAwesomeIcon icon={faEnvelope} size='3x' />
+				</a>
+			</div>
+			<div className='icon'>
+				<a href='https://www.facebook.com/eastsideswimschool/' target='_blank'>
+					<FontAwesomeIcon icon={faFacebook} size='3x' />
+				</a>
+			</div>
 		</div>
 	</footer>
 );
