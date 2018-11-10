15e6f750946cdc56f948eac43bd89a5878fc9982
Lauren <llwor94@gmail.com>
2018-11-09 20:45:40 -0800

    fixed not found page to redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { Redirect } from '@reach/router';
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+	<Redirect to='/' />
+	// <Layout>
+	//   <h1>NOT FOUND</h1>
+	//   <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+	// </Layout>
 );
 
 export default NotFoundPage;
