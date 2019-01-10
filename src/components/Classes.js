9283634b008525e6fda441dfe990c80b34f732e3
Lauren <llwor94@gmail.com>
2019-01-09 19:27:31 -0800

    created instruction modals

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -3,202 +3,261 @@ import { StaticQuery, graphql } from 'gatsby';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import GridCard from './GridCard';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Tooltip from '@material-ui/core/Tooltip';
+import InfoIcon from '@material-ui/icons/Info';
 import styles from './Classes.module.styl';
 
-const Classes = () => (
-	<StaticQuery
-		query={graphql`
-			query {
-				allMarkdownRemark(
-					filter: { fileAbsolutePath: { regex: "/classes/" } }
-					sort: { fields: frontmatter___difficulty }
-				) {
-					edges {
-						node {
-							html
-							frontmatter {
-								title
-								time
-								day
-								time2
-								day2
-								time3
-								day3
-								ageGroup
-								difficulty
-								thumbnail
-								link
+class Classes extends React.Component {
+	state = {
+		open: false,
+	};
+	render() {
+		return (
+			<StaticQuery
+				query={graphql`
+					query {
+						allMarkdownRemark(
+							filter: { fileAbsolutePath: { regex: "/classes/" } }
+							sort: { fields: frontmatter___difficulty }
+						) {
+							edges {
+								node {
+									html
+									frontmatter {
+										title
+										time
+										day
+										time2
+										day2
+										time3
+										day3
+										ageGroup
+										difficulty
+										thumbnail
+										link
+									}
+								}
 							}
 						}
-					}
-				}
-				allFile(filter: { sourceInstanceName: { regex: "/cms/" } }) {
-					edges {
-						node {
-							childImageSharp {
-								fluid(maxWidth: 1200, jpegProgressive: true) {
-									srcSet
-									src
-									sizes
-									originalName
+						allFile(filter: { sourceInstanceName: { regex: "/cms/" } }) {
+							edges {
+								node {
+									childImageSharp {
+										fluid(maxWidth: 1200, jpegProgressive: true) {
+											srcSet
+											src
+											sizes
+											originalName
+										}
+									}
 								}
 							}
 						}
 					}
-				}
-			}
-		`}
-		render={({ allMarkdownRemark, allFile }) => {
-			// console.log(allMarkdownRemark)
+				`}
+				render={({ allMarkdownRemark, allFile }) => {
+					// console.log(allMarkdownRemark)
 
-			return (
-				<section className={styles.wrapper} name='classes'>
-					<Typography variant='h2' component='h2' gutterBottom>
-						Group Classes
-					</Typography>
+					return (
+						<section className={styles.wrapper} name='classes'>
+							<Dialog
+								onClose={() => this.setState({ open: false })}
+								aria-labelledby='customized-dialog-title'
+								open={this.state.open}
+							>
+								<MuiDialogTitle disableTypography>
+									<Typography variant='h6'>How To Register</Typography>
 
-					<div className={styles.grid}>
-						<Grid container spacing={40} alignItems='stretch'>
-							{allMarkdownRemark.edges
-								.filter(({ node }) => node.frontmatter.ageGroup === 'Preschool')
-								.map(({ node }) => (
-									<GridCard
-										time={
-											node.frontmatter.day ? (
-												`${node.frontmatter.day.slice(0, 3)} ${node
-													.frontmatter.time}`
-											) : (
-												'We recommend private instruction.'
-											)
-										}
-										time2={
-											node.frontmatter.time2 &&
-											`${node.frontmatter.day2.slice(0, 3)} ${node.frontmatter
-												.time2}`
-										}
-										key={node.frontmatter.title}
-										img={allFile.edges.find(({ node: file }) => {
-											return (
-												file.childImageSharp.fluid.originalName ===
-												node.frontmatter.thumbnail.split('/')[2]
-											);
-										})}
-										header={node.frontmatter.title}
-										level={node.frontmatter.difficulty}
-										link={node.frontmatter.link}
-										ages='3-5'
-										text={node.html}
-										color='ps'
-									/>
-								))}
-						</Grid>
+									<IconButton
+										aria-label='Close'
+										onClick={() => this.setState({ open: false })}
+										style={{ position: 'absolute', top: 3, right: 3 }}
+									>
+										<CloseIcon />
+									</IconButton>
+								</MuiDialogTitle>
+								<MuiDialogContent>
+									<Typography gutterBottom variant='body1' component='p'>
+										All registration is handled online through Wellness Living.
+										You can access this directly or via our website.
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										To register you will need to create an account. If you have
+										more than one person from the same family taking lessons you
+										do not need multiple accounts, simply add the names of the
+										students to your profile.
+									</Typography>
+								</MuiDialogContent>
+							</Dialog>
+							<Typography variant='h2' component='h2' gutterBottom>
+								Group Classes{' '}
+								<Tooltip title='How to register' placement='top'>
+									<IconButton onClick={() => this.setState({ open: true })}>
+										<InfoIcon
+											color='disabled'
+											className={styles.icon}
+											aria-label='How to register'
+										/>
+									</IconButton>
+								</Tooltip>
+							</Typography>
 
-						<Grid container spacing={40} className={styles.grid}>
-							{allMarkdownRemark.edges
-								.filter(({ node }) => node.frontmatter.ageGroup === 'Grade School')
-								.map(({ node }) => (
-									<GridCard
-										time={`${node.frontmatter.day.slice(0, 3)} ${node
-											.frontmatter.time}`}
-										time2={
-											node.frontmatter.time2 &&
-											`${node.frontmatter.day2.slice(0, 3)} ${node.frontmatter
-												.time2}`
-										}
-										time3={
-											node.frontmatter.time3 &&
-											`${node.frontmatter.day3.slice(0, 3)} ${node.frontmatter
-												.time3}`
-										}
-										img={allFile.edges.find(({ node: file }) => {
-											return (
-												file.childImageSharp.fluid.originalName ===
-												node.frontmatter.thumbnail.split('/')[2]
-											);
-										})}
-										header={node.frontmatter.title}
-										level={node.frontmatter.difficulty}
-										ages='5-12'
-										key={node.frontmatter.title}
-										link={node.frontmatter.link}
-										text={node.html}
-										color='gs'
-									/>
-								))}
-						</Grid>
+							<div className={styles.grid}>
+								<Grid container spacing={40} alignItems='stretch'>
+									{allMarkdownRemark.edges
+										.filter(
+											({ node }) => node.frontmatter.ageGroup === 'Preschool',
+										)
+										.map(({ node }) => (
+											<GridCard
+												time={
+													node.frontmatter.day ? (
+														`${node.frontmatter.day.slice(0, 3)} ${node
+															.frontmatter.time}`
+													) : (
+														'We recommend private instruction.'
+													)
+												}
+												time2={
+													node.frontmatter.time2 &&
+													`${node.frontmatter.day2.slice(0, 3)} ${node
+														.frontmatter.time2}`
+												}
+												key={node.frontmatter.title}
+												img={allFile.edges.find(({ node: file }) => {
+													return (
+														file.childImageSharp.fluid.originalName ===
+														node.frontmatter.thumbnail.split('/')[2]
+													);
+												})}
+												header={node.frontmatter.title}
+												level={node.frontmatter.difficulty}
+												link={node.frontmatter.link}
+												ages='3-5'
+												text={node.html}
+												color='ps'
+											/>
+										))}
+								</Grid>
 
-						<Grid container spacing={40} className={styles.grid}>
-							{allMarkdownRemark.edges
-								.filter(({ node }) => node.frontmatter.ageGroup === 'Pre Comp')
-								.map(({ node }) => (
-									<GridCard
-										time={`${node.frontmatter.day.slice(0, 3)} ${node
-											.frontmatter.time}`}
-										time2={
-											node.frontmatter.time2 &&
-											`${node.frontmatter.day2.slice(0, 3)} ${node.frontmatter
-												.time2}`
-										}
-										time3={
-											node.frontmatter.time3 &&
-											`${node.frontmatter.day3.slice(0, 3)} ${node.frontmatter
-												.time3}`
-										}
-										img={allFile.edges.find(({ node: file }) => {
-											return (
-												file.childImageSharp.fluid.originalName ===
-												node.frontmatter.thumbnail.split('/')[2]
-											);
-										})}
-										header={node.frontmatter.title}
-										ages='14 and under'
-										level={node.frontmatter.difficulty}
-										key={node.frontmatter.title}
-										link={node.frontmatter.link}
-										text={node.html}
-										color='jh'
-									/>
-								))}
-						</Grid>
+								<Grid container spacing={40} className={styles.grid}>
+									{allMarkdownRemark.edges
+										.filter(
+											({ node }) =>
+												node.frontmatter.ageGroup === 'Grade School',
+										)
+										.map(({ node }) => (
+											<GridCard
+												time={`${node.frontmatter.day.slice(0, 3)} ${node
+													.frontmatter.time}`}
+												time2={
+													node.frontmatter.time2 &&
+													`${node.frontmatter.day2.slice(0, 3)} ${node
+														.frontmatter.time2}`
+												}
+												time3={
+													node.frontmatter.time3 &&
+													`${node.frontmatter.day3.slice(0, 3)} ${node
+														.frontmatter.time3}`
+												}
+												img={allFile.edges.find(({ node: file }) => {
+													return (
+														file.childImageSharp.fluid.originalName ===
+														node.frontmatter.thumbnail.split('/')[2]
+													);
+												})}
+												header={node.frontmatter.title}
+												level={node.frontmatter.difficulty}
+												ages='5-12'
+												key={node.frontmatter.title}
+												link={node.frontmatter.link}
+												text={node.html}
+												color='gs'
+											/>
+										))}
+								</Grid>
 
-						<Grid container spacing={40} className={styles.grid}>
-							{allMarkdownRemark.edges
-								.filter(({ node }) => node.frontmatter.ageGroup === 'Adult')
-								.map(({ node }) => (
-									<GridCard
-										time={`${node.frontmatter.day.slice(0, 3)} ${node
-											.frontmatter.time}`}
-										time2={
-											node.frontmatter.time2 &&
-											`${node.frontmatter.day2.slice(0, 3)} ${node.frontmatter
-												.time2}`
-										}
-										time3={
-											node.frontmatter.time3 &&
-											`${node.frontmatter.day3.slice(0, 3)} ${node.frontmatter
-												.time3}`
-										}
-										img={allFile.edges.find(({ node: file }) => {
-											return (
-												file.childImageSharp.fluid.originalName ===
-												node.frontmatter.thumbnail.split('/')[2]
-											);
-										})}
-										header={node.frontmatter.title}
-										ages='18 +'
-										level={node.frontmatter.difficulty}
-										key={node.frontmatter.title}
-										link={node.frontmatter.link}
-										text={node.html}
-										color='ad'
-									/>
-								))}
-						</Grid>
-					</div>
-				</section>
-			);
-		}}
-	/>
-);
+								<Grid container spacing={40} className={styles.grid}>
+									{allMarkdownRemark.edges
+										.filter(
+											({ node }) => node.frontmatter.ageGroup === 'Pre Comp',
+										)
+										.map(({ node }) => (
+											<GridCard
+												time={`${node.frontmatter.day.slice(0, 3)} ${node
+													.frontmatter.time}`}
+												time2={
+													node.frontmatter.time2 &&
+													`${node.frontmatter.day2.slice(0, 3)} ${node
+														.frontmatter.time2}`
+												}
+												time3={
+													node.frontmatter.time3 &&
+													`${node.frontmatter.day3.slice(0, 3)} ${node
+														.frontmatter.time3}`
+												}
+												img={allFile.edges.find(({ node: file }) => {
+													return (
+														file.childImageSharp.fluid.originalName ===
+														node.frontmatter.thumbnail.split('/')[2]
+													);
+												})}
+												header={node.frontmatter.title}
+												ages='14 and under'
+												level={node.frontmatter.difficulty}
+												key={node.frontmatter.title}
+												link={node.frontmatter.link}
+												text={node.html}
+												color='jh'
+											/>
+										))}
+								</Grid>
+
+								<Grid container spacing={40} className={styles.grid}>
+									{allMarkdownRemark.edges
+										.filter(({ node }) => node.frontmatter.ageGroup === 'Adult')
+										.map(({ node }) => (
+											<GridCard
+												time={`${node.frontmatter.day.slice(0, 3)} ${node
+													.frontmatter.time}`}
+												time2={
+													node.frontmatter.time2 &&
+													`${node.frontmatter.day2.slice(0, 3)} ${node
+														.frontmatter.time2}`
+												}
+												time3={
+													node.frontmatter.time3 &&
+													`${node.frontmatter.day3.slice(0, 3)} ${node
+														.frontmatter.time3}`
+												}
+												img={allFile.edges.find(({ node: file }) => {
+													return (
+														file.childImageSharp.fluid.originalName ===
+														node.frontmatter.thumbnail.split('/')[2]
+													);
+												})}
+												header={node.frontmatter.title}
+												ages='18 +'
+												level={node.frontmatter.difficulty}
+												key={node.frontmatter.title}
+												link={node.frontmatter.link}
+												text={node.html}
+												color='ad'
+											/>
+										))}
+								</Grid>
+							</div>
+						</section>
+					);
+				}}
+			/>
+		);
+	}
+}
 
 export default Classes;
--- src/components/Instructors.js
+++ src/components/Instructors.js
@@ -5,8 +5,19 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Tooltip from '@material-ui/core/Tooltip';
+import InfoIcon from '@material-ui/icons/Info';
 import styles from './Instructors.module.styl';
+
+import classesView from '../images/classesView.png';
+import calendarView from '../images/calendarView.png';
+import finishedView from '../images/finishedView.png';
+import timeView from '../images/timeView.png';
 
 const BottomBar = ({ styles, link }) => (
 	<div>
@@ -23,131 +34,239 @@ const BottomBar = ({ styles, link }) => (
 	</div>
 );
 
-const Instructors = () => (
-	<StaticQuery
-		query={graphql`
-			query {
-				allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/coaches/" } }) {
-					edges {
-						node {
-							html
-							frontmatter {
-								title
-								oneStudent
-								twoStudents
-								thumbnail
-								link
+class Instructors extends React.Component {
+	state = {
+		open: false,
+	};
+	render() {
+		return (
+			<StaticQuery
+				query={graphql`
+					query {
+						allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/coaches/" } }) {
+							edges {
+								node {
+									html
+									frontmatter {
+										title
+										oneStudent
+										twoStudents
+										thumbnail
+										link
+									}
+								}
 							}
 						}
 					}
-				}
-			}
-		`}
-		render={({ allMarkdownRemark }) => {
-			// console.log(allMarkdownRemark);
-			// const { classes } = props
-			allMarkdownRemark.edges.sort(
-				(
-					{ node: { frontmatter: { title: title1 } } },
-					{ node: { frontmatter: { title: title2 } } },
-				) => {
-					if (title1 === 'Lisa') return -1;
-					if (title2 === 'Lisa') return 1;
+				`}
+				render={({ allMarkdownRemark }) => {
+					// console.log(allMarkdownRemark);
+					// const { classes } = props
+					allMarkdownRemark.edges.sort(
+						(
+							{ node: { frontmatter: { title: title1 } } },
+							{ node: { frontmatter: { title: title2 } } },
+						) => {
+							if (title1 === 'Lisa') return -1;
+							if (title2 === 'Lisa') return 1;
 
-					return title1.localeCompare(title2);
-				},
-			);
-			return (
-				<section className={styles.instructors} name='coaches'>
-					<div className={styles.container}>
-						<Paper className={styles.instructor}>
-							<div className={styles.bline}>
-								<Typography gutterBottom variant='h2' component='h2'>
-									Private Lessons
-								</Typography>
-							</div>
+							return title1.localeCompare(title2);
+						},
+					);
+					return (
+						<section className={styles.instructors} name='coaches'>
+							<Dialog
+								onClose={() => this.setState({ open: false })}
+								aria-labelledby='customized-dialog-title'
+								open={this.state.open}
+							>
+								<MuiDialogTitle disableTypography>
+									<Typography variant='h6'>How To Register</Typography>
 
-							<Grid container spacing={40}>
-								{allMarkdownRemark.edges.map(
-									(
-										{
-											node: {
-												frontmatter: {
-													thumbnail,
-													title,
-													oneStudent,
-													twoStudents,
-													link,
+									<IconButton
+										aria-label='Close'
+										onClick={() => this.setState({ open: false })}
+										style={{ position: 'absolute', top: 3, right: 3 }}
+									>
+										<CloseIcon />
+									</IconButton>
+								</MuiDialogTitle>
+								<MuiDialogContent>
+									<Typography gutterBottom variant='body1' component='p'>
+										All registration is handled online through Wellness Living.
+										You can access this directly or via our website.
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										To register you will need to create an account. If you have
+										more than one person from the same family taking lessons you
+										do not need multiple accounts, simply add the names of the
+										students to your profile.
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										Booking for solo and pair private lessons is a done on a
+										per-month basis. The day and time you select are yours for
+										the entire month. You may also keep the same day and time
+										for subsequent months.
+									</Typography>
+									<Typography gutterBottom variant='subtitle1' component='h2'>
+										How to register for solo or pair private lessons:
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										You must have an account created on the Wellness Living
+										site. If you don’t already, you will be prompted during the
+										registration process.
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										Choose the instructor that you would like to work with and
+										then click on the registration tab under their name. It will
+										show “appointment booking” at the top of page. Find the
+										instructor and click “privates” (these are the solo lessons)
+										or “semi-privates” (these are the pair lessons) The price
+										will be listed.
+									</Typography>
+									<img src={classesView} className={styles.modalImg} />
+									<Typography gutterBottom variant='body1' component='p'>
+										Click “NEXT” on bottom right of page. This will take you to
+										the calendar.
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										Click forward to upcoming month and then find a day that is
+										bolded. The non highlighted days denote that the instructor
+										is not available.
+									</Typography>
+									<img src={calendarView} className={styles.modalImg} />
+									<Typography gutterBottom variant='body1' component='p'>
+										Select a day and the system will display the available
+										times. Select a time.
+									</Typography>
+									<img src={timeView} className={styles.modalImg} />
+									<Typography gutterBottom variant='body1' component='p'>
+										Please start with a day at the beginning of the month. Once
+										you have made your selection this day and time is locked in
+										for you personally for the rest of the month. You may also
+										keep this day and time for subsequent months.
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										Make sure the “Book Recurring” option is switched to “On.”
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										For the “Repeats Every” dropdowns select “1” and “Week.”,
+										and ensure the day of the week you selected on the calendar
+										is selected.
+									</Typography>
+									<Typography gutterBottom variant='body1' component='p'>
+										For the “Ends” option, ensure “On Date” is selected. Click
+										on the Calendar Icon and select the end of the current month
+										or a subsequent month (up to three).
+									</Typography>
+									<img src={finishedView} className={styles.modalImg} />
+								</MuiDialogContent>
+							</Dialog>
+							<div className={styles.container}>
+								<Paper className={styles.instructor}>
+									<div className={styles.bline}>
+										<Typography gutterBottom variant='h2' component='h2'>
+											Private Lessons
+											<Tooltip title='How to register' placement='top'>
+												<IconButton
+													onClick={() => this.setState({ open: true })}
+												>
+													<InfoIcon
+														color='disabled'
+														className={styles.icon}
+														aria-label='How to register'
+													/>
+												</IconButton>
+											</Tooltip>
+										</Typography>
+									</div>
+
+									<Grid container spacing={40}>
+										{allMarkdownRemark.edges.map(
+											(
+												{
+													node: {
+														frontmatter: {
+															thumbnail,
+															title,
+															oneStudent,
+															twoStudents,
+															link,
+														},
+														html,
+													},
 												},
-												html,
-											},
-										},
-										idx,
-									) => (
-										<Grid
-											key={title}
-											className={styles.bottomLineWrapper}
-											item
-											xs={12}
-										>
-											<Grid
-												container
-												spacing={40}
-												className={styles.bottomLine}
-											>
+												idx,
+											) => (
 												<Grid
+													key={title}
+													className={styles.bottomLineWrapper}
 													item
-													className={styles.grow}
 													xs={12}
-													sm={7}
-													md={8}
 												>
-													<div className={styles.wrapper}>
-														<Typography
-															gutterBottom
-															variant='h5'
-															component='h3'
+													<Grid
+														container
+														spacing={40}
+														className={styles.bottomLine}
+													>
+														<Grid
+															item
+															className={styles.grow}
+															xs={12}
+															sm={7}
+															md={8}
 														>
-															{title}
-														</Typography>
-														<Typography
-															gutterBottom
-															variant='body1'
-															component='div'
-															dangerouslySetInnerHTML={{
-																__html: html,
-															}}
-														/>
-														<BottomBar styles={styles} link={link} />
-													</div>
+															<div className={styles.wrapper}>
+																<Typography
+																	gutterBottom
+																	variant='h5'
+																	component='h3'
+																>
+																	{title}
+																</Typography>
+																<Typography
+																	gutterBottom
+																	variant='body1'
+																	component='div'
+																	dangerouslySetInnerHTML={{
+																		__html: html,
+																	}}
+																/>
+																<BottomBar
+																	styles={styles}
+																	link={link}
+																/>
+															</div>
+														</Grid>
+														<Grid
+															item
+															className={`${idx % 2 === 0 &&
+																styles.first} ${styles.pic}`}
+															xs={12}
+															sm={5}
+															md={4}
+														>
+															<div className={styles.imgWrapper}>
+																<img
+																	src={thumbnail}
+																	alt={title}
+																	className={styles.img}
+																/>
+															</div>
+														</Grid>
+													</Grid>
 												</Grid>
-												<Grid
-													item
-													className={`${idx % 2 === 0 &&
-														styles.first} ${styles.pic}`}
-													xs={12}
-													sm={5}
-													md={4}
-												>
-													<div className={styles.imgWrapper}>
-														<img
-															src={thumbnail}
-															alt={title}
-															className={styles.img}
-														/>
-													</div>
-												</Grid>
-											</Grid>
-										</Grid>
-									),
-								)}
-							</Grid>
-						</Paper>
-					</div>
-				</section>
-			);
-		}}
-	/>
-);
+											),
+										)}
+									</Grid>
+								</Paper>
+							</div>
+						</section>
+					);
+				}}
+			/>
+		);
+	}
+}
 
 export default Instructors;
