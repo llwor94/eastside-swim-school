4034db28216f8d675600d149368accf2cd60bab7
Lauren <llwor94@gmail.com>
2019-12-25 22:24:44 -0800

    notifications n stuff

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,70 +1,38 @@
-import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
-import Snackbar from '@material-ui/core/Snackbar';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import React, { useEffect } from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { useSnackbar } from 'notistack';
 
-class Announcement extends React.Component {
-	state = {
-		open: true,
-	};
-	render() {
-		return (
-			<StaticQuery
-				query={graphql`
-					query {
-						allMarkdownRemark(
-							filter: { fileAbsolutePath: { regex: "/announcements/" } }
-						) {
-							edges {
-								node {
-									html
-									frontmatter {
-										title
-									}
-								}
-							}
-						}
-					}
-				`}
-				render={({ allMarkdownRemark }) => {
-					return (
-						<Snackbar
-							anchorOrigin={{
-								vertical: 'bottom',
-								horizontal: 'center',
-							}}
-							open={this.state.open}
-							onClose={() => this.setState({ open: false })}
-						>
-							<SnackbarContent
-								message={
-									<span>{allMarkdownRemark.edges[0].node.frontmatter.title}</span>
-								}
-								style={{
-									maxWidth: '100%',
-									width: '100vh',
-									flexWrap: 'nowrap',
-									padding: '10px 24px 20px',
-								}}
-								action={[
-									<IconButton
-										key='close'
-										aria-label='Close'
-										color='inherit'
-										onClick={() => this.setState({ open: false })}
-									>
-										<CloseIcon />
-									</IconButton>,
-								]}
-							/>
-						</Snackbar>
-					);
-				}}
-			/>
-		);
-	}
-}
+const Announcement = ({ children }) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/announcements/" } }
+      ) {
+        edges {
+          node {
+            html
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  useEffect(() => {
+    if (allMarkdownRemark) {
+      allMarkdownRemark.edges.forEach(({ node }) => {
+        enqueueSnackbar(node.frontmatter.title, {
+          persist: true,
+          preventDuplicate: true,
+        });
+      });
+    }
+  }, [allMarkdownRemark]);
+
+  return children;
+};
 
 export default Announcement;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useRef } from "react";
 import Layout from '../components/Layout';
-
+import { SnackbarProvider } from 'notistack';
 import Jumbotron from '../components/Jumbotron';
 import Classes from '../components/Classes';
+import CloseIcon from '@material-ui/icons/Close';
 import Instructors from '../components/Instructors';
 import Testimonial from '../components/Testimonials';
-import Announcement from '../components/Announcement'
+import Announcement from '../components/Announcement';
 
-class IndexPage extends React.Component {
+const IndexPage = () => {
+  const notistackRef = useRef();
+  const onClickDismiss = key => () => {
+    notistackRef.current.closeSnackbar(key);
+  }
+  return (
+    <Layout>
+      <SnackbarProvider  ref={notistackRef} maxSnack={5} action={(key) => (
+        <CloseIcon onClick={onClickDismiss(key)} />
 
-	render() {
-		return (
-			<Layout>
-				<Jumbotron />
-				<Classes />
-				<Instructors />
-				<Testimonial />
-				<Announcement />
-			</Layout>
-		);
-	}
+      )}>
+        <Announcement >
+          <Jumbotron />
+          <Classes />
+          <Instructors />
+          <Testimonial />
+        </Announcement>
+      </SnackbarProvider>
+    </Layout>
+  );
 }
 
 export default IndexPage;
