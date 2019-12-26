193a4e59da32db24103ded5d6121698320b32332
Lauren <llwor94@gmail.com>
2019-12-25 23:14:01 -0800

    workin on it

--- src/templates/custom-page.js
+++ src/templates/custom-page.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { graphql } from 'gatsby';
 import styles from './index.module.styl';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
-export default () => {
+export default ({data}) => {
+  console.log(data)
   return (
     <Layout>
-      <div className={styles.wrapper}>Hello blog post</div>
+      <div className={styles.wrapper}>
+        <Paper>
+          <Typography style={{color: 'rgba(0, 0, 0, 0.87)', textTransform: "capitalize"}} gutterBottom variant='h2' component='h1'>
+            {data.markdownRemark.frontmatter.title}
+          </Typography>
+          <div className={styles.paper} dangerouslySetInnerHTML={{__html: data.markdownRemark.html}} />
+        </Paper>
+      </div>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
