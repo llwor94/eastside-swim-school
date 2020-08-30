fa31c3d7de27906cc2de91728d647e4260f1be5a
Lauren <llwor94@gmail.com>
2020-08-30 15:42:08 -0700

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,81 +7,86 @@ require('dotenv').config({
 	path: `.env.${activeEnv}`,
 });
 
+const PLUGINS = [
+  'gatsby-plugin-react-helmet',
+  `gatsby-plugin-styled-components`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `cms-images`,
+      path: `${__dirname}/static/assets/`,
+    },
+  },
+  'gatsby-transformer-sharp',
+  'gatsby-plugin-sharp',
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: 'gatsby-starter-default',
+      short_name: 'starter',
+      start_url: '/',
+      background_color: '#663399',
+      theme_color: '#663399',
+      display: 'minimal-ui',
+      icon: 'src/assets/favicon.png', // This path is relative to the root of the site.
+    },
+  },
+  'gatsby-plugin-offline',
+  'gatsby-plugin-netlify-cms',
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `testimonials`,
+      path: `${__dirname}/testimonials/`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `pages`,
+      path: `${__dirname}/pages/`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `classes`,
+      path: `${__dirname}/classes/`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `coaches`,
+      path: `${__dirname}/coaches/`,
+    },
+  },
+  'gatsby-transformer-remark',
+  'gatsby-plugin-stylus',
+  '@wapps/gatsby-plugin-material-ui',
+];
+
+if (existsSync(`${__dirname}/announcements`) {
+  PLUGINS.push({
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `announcements`,
+      path: `${__dirname}/announcements/`,
+    },
+  })
+}
+
 module.exports = {
   siteMetadata: {
     title: 'Swim Lessons in Redmond | Eastside Swim School',
     description: 'Swim School in Redmond, WA',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `cms-images`,
-        path: `${__dirname}/static/assets/`,
-      },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/favicon.png', // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify-cms',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `testimonials`,
-        path: `${__dirname}/testimonials/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/pages/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `classes`,
-        path: `${__dirname}/classes/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `coaches`,
-        path: `${__dirname}/coaches/`,
-      },
-    },
-    existsSync(`${__dirname}/announcements`) && {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `announcements`,
-        path: `${__dirname}/announcements/`,
-      },
-    },
-    'gatsby-transformer-remark',
-    'gatsby-plugin-stylus',
-    '@wapps/gatsby-plugin-material-ui',
-  ],
+  plugins: PLUGINS
 };
